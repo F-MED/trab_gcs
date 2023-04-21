204fe2f5bbf7956db402893d5ef1cdea13bebf98
Brunoduartrosa <119279994+Brunoduartrosa@users.noreply.github.com>
2023-04-21 17:40:24 -0300

    Add files via upload

---
+++ Trabalho1/src/Departamento.java
@@ -0,0 +1,12 @@
+import java.util.Arraylist;
+Public class Departamento{
+    private Arraylist<Funcionarios> funcionarios;
+    private Arraylist<Equipamentos> equipamentos;
+    private String nome;
+    private int idDepart;
+
+
+
+
+
+}
